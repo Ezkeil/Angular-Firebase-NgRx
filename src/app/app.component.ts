ab30feb041c4ac46e9bfd4813419c62a7d997df5
Tran Thanh Binh <tranthanhbinh10973@gmail.com>
2022-09-27 16:59:21 +0700

    Name: Test accessing child with ViewChild and AfterViewInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ViewChild,
+} from '@angular/core';
+import { DisplayCountComponent } from './display-count/display-count.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit, AfterViewChecked {
+  @ViewChild(DisplayCountComponent) viewChild!: DisplayCountComponent;
   title = 'angular-firebase-ngrx';
   count = 0;
   person = {
     name: 'John',
     age: 23,
   };
+  private inputValue = '';
+
+  ngAfterViewInit(): void {}
+
+  ngAfterViewChecked(): void {
+    if (this.inputValue !== this.viewChild.test1)
+      this.inputValue = this.viewChild.test1;
+  }
 
   onIncrement(payload: number) {
     this.count += payload;
